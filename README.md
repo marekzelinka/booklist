b687eb8cb7127504d849c8c90676e9633f3e70aa
Marek Zelinka <mzelinka17@gmail.com>
2025-12-18 14:51:07 +0100

    using dataclasses

---
+++ README.md
@@ -0,0 +1,6 @@
+# Booklist
+
+## Pydantic
+
+- with dataclasses, we need to pass in exact arguments, and we have no way to checking if the argument is the right type we expect
+- with pydantic we validate each item, making sure that the field are in the required format.
--- main.py
+++ main.py
@@ -1,5 +1,41 @@
+import json
+from dataclasses import dataclass
+
+
+@dataclass
+class Book:
+    """Models a book that you can read from a file."""
+
+    title: str
+    subtitle: str
+    author: str
+    publisher: str
+    isbn_10: str
+    isbn_13: str
+    price: float
+
+
 def main():
-    print("Hello from booklist!")
+    """
+    Basic example showing how to read and validate data from a file using Pydantic.
+    """
+
+    # read books from json and print
+    with open("data.json") as file:
+        data = json.load(file)
+        books = [
+            Book(
+                title=item.get("title", ""),
+                subtitle=item.get("subtitle", ""),
+                author=item.get("author", ""),
+                publisher=item.get("publisher", ""),
+                isbn_10=item.get("isbn_10", ""),
+                isbn_13=item.get("isbn_13", ""),
+                price=item.get("price", 0),
+            )
+            for item in data
+        ]
+        print(books[0])
 
 
 if __name__ == "__main__":
