c8ffb80cbe9730261cb0999818ec457df64077bf
Marek Zelinka <mzelinka17@gmail.com>
2025-12-18 14:51:49 +0100

    using pydantic

--- main.py
+++ main.py
@@ -1,18 +1,81 @@
 import json
-from dataclasses import dataclass
+import string
+from typing import Any
+
+from pydantic import (
+    BaseModel,
+    PositiveFloat,
+    field_validator,
+    model_validator,
+)
 
 
-@dataclass
-class Book:
+class ISBNMissingError(ValueError):
+    """Custom error that is raised when both ISBN10 and ISBN13 are missing."""
+
+    def __init__(self, title: str, message: str) -> None:
+        self.title = title
+        super().__init__(message)
+
+
+class ISBN10FormatError(ValueError):
+    """Custom error that is raised when ISBN10 is in invalid format."""
+
+    def __init__(self, value: str, message: str) -> None:
+        self.value = value
+        super().__init__(message)
+
+
+class Author(BaseModel):
+    name: str
+    verified: bool
+
+
+class Book(BaseModel, frozen=True, str_to_lower=True):
     """Models a book that you can read from a file."""
 
     title: str
-    subtitle: str
+    subtitle: str | None = None
     author: str
+    author2: Author | None = None
     publisher: str
-    isbn_10: str
-    isbn_13: str
-    price: float
+    price: PositiveFloat
+    isbn_10: str | None = None
+    isbn_13: str | None = None
+
+    @model_validator(mode="before")
+    @classmethod
+    def check_isbn_10_or_13(cls, data: Any) -> Any:
+        """Checks that there is either an isbn_10 and/or isbn_13 value defined."""
+        if isinstance(data, dict):
+            if "isbn_10" not in data and "isbn_13" not in data:
+                raise ISBNMissingError(
+                    title=data["title"],
+                    message="Document should have either an ISBN10 and/or ISBN13.",
+                )
+        return data
+
+    @field_validator("isbn_10")
+    @classmethod
+    def isbn_10_valid(cls, value: str) -> str:
+        """Checks if the isbn_10 is a valid ISBN."""
+        chars = [char for char in value if char in string.digits + "Xx"]
+        if len(chars) != 10:
+            raise ISBN10FormatError(
+                value=value, message="ISBN10 should be 10 character long/"
+            )
+        if (
+            sum(
+                (10 - i) * (10 if char in "Xx" else int(char))
+                for i, char in enumerate(chars)
+            )
+            % 11
+            != 0
+        ):
+            raise ISBN10FormatError(
+                value=value, message="ISBN10 is invalid ISBN format."
+            )
+        return value
 
 
 def main():
@@ -23,18 +86,7 @@ def main():
     # read books from json and print
     with open("data.json") as file:
         data = json.load(file)
-        books = [
-            Book(
-                title=item.get("title", ""),
-                subtitle=item.get("subtitle", ""),
-                author=item.get("author", ""),
-                publisher=item.get("publisher", ""),
-                isbn_10=item.get("isbn_10", ""),
-                isbn_13=item.get("isbn_13", ""),
-                price=item.get("price", 0),
-            )
-            for item in data
-        ]
+        books = [Book(**item) for item in data]
         print(books[0])
 
 
